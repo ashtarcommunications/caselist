e946ed85dcc8da53171e5f2dc647206632865e39
Aaron Hardy <ashtarcommunications@gmail.com>
2021-10-15 22:57:46 -0600

    Works in a route

--- client/src/Home.js
+++ client/src/Home.js
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { loadCaselist } from './api';
 
 const Home = () => {
+    const [caselistData, setCaselistData] = useState({});
+
+    const { caselist } = useParams();
+    console.log(caselist);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                if (caselist) {
+                    const response = await loadCaselist(caselist);
+                    setCaselistData(response || {});
+                }
+            } catch (err) {
+                setCaselistData({});
+                console.log(err);
+            }
+        };
+        fetchData();
+    }, [caselist]);
+
     return (
         <div className="home">
             <h1>openCaselist</h1>
+            <h2>Current Caselist: {JSON.stringify(caselistData)}</h2>
         </div>
     );
 };
