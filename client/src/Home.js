8c4b20a9260540805b66275b9ffdb1b783c8225c
Aaron Hardy <ashtarcommunications@gmail.com>
2021-10-30 17:59:49 -0600

    Icons, homepage

--- client/src/Home.js
+++ client/src/Home.js
@@ -4,7 +4,17 @@ import './Home.css';
 const Home = () => {
     return (
         <div className="home">
-            <h1>openCaselist Homepage</h1>
+            <h1>Welcome to openCaselist</h1>
+            <h2>The home for collaborative disclosure for the debate community</h2>
+
+            <div className="caselists">
+                <div className="caselist"><span>NDT-CEDA 2021-2022</span></div>
+                <div className="caselist"><span>HS Policy 2021-2022</span></div>
+                <div className="caselist"><span>HS LD 2021-2022</span></div>
+                <div className="caselist"><span>HS PF 2021-2022</span></div>
+                <div className="caselist"><span>NFA-LD 2021-2022</span></div>
+                <div className="caselist"><span>Open Evidence Project</span></div>
+            </div>
         </div>
     );
 };
