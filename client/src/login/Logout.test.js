162bb9bb32910cccf0c6db9d00045ee86be25c1c
Aaron Hardy <ashtarcommunications@gmail.com>
2022-06-15 11:11:50 -0600

    More tests

---
+++ client/src/login/Logout.test.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { assert } from 'chai';
+
+import { wrappedRender as render, waitFor } from '../setupTests';
+// eslint-disable-next-line import/named
+import { auth } from '../helpers/auth';
+
+import Logout from './Logout';
+
+jest.mock('react-router-dom', () => ({
+    ...jest.requireActual('react-router-dom'),
+    useNavigate: jest.fn().mockImplementation(() => () => true),
+    Navigate: () => true,
+}));
+
+describe('Logout', () => {
+    it('Logs out the user', async () => {
+        render(<Logout />);
+        await waitFor(() => assert.strictEqual(auth.handleLogout.mock.calls.length, 1, 'Logged out'));
+    });
+
+    afterEach(() => {
+        auth.handleLogout.mockReset();
+    });
+});
