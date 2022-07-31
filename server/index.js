9910e0b6a0e99b2d2f15f061dda8a3cef540f3ba
Aaron Hardy <ashtarcommunications@gmail.com>
2022-07-31 15:15:04 -0600

    No rate limiting for admins

--- server/index.js
+++ server/index.js
@@ -99,7 +99,7 @@ const speedLimiter = slowDown({
     delayMs: config.SLOWDOWN_RATE_DELAY || 50, // Add 50ms of delay per request above 1000
     maxDelayMs: config.SLOWDOWN_MAX_DELAY || 10000, // Cap max delay at 10s per request
     keyGenerator: (req) => (req.user_id ? req.user_id : req.ip),
-    skip: req => req.method === 'OPTIONS' || process.env.NODE_ENV === 'test',
+    skip: req => req.method === 'OPTIONS' || process.env.NODE_ENV === 'test' || config.ADMINS?.includes(req.user_id),
 });
 
 // Rate limit all requests
@@ -111,7 +111,7 @@ const getLimiter = rateLimiter({
         debugLogger.info(`1000 requests/15m rate limit enforced on user ${req.user_id}`);
         res.status(429).send({ message: 'You have exceeded the allowed number of page views per 15 minutes. Wait and try again.' });
     },
-    skip: req => req.method === 'OPTIONS' || process.env.NODE_ENV === 'test',
+    skip: req => req.method === 'OPTIONS' || process.env.NODE_ENV === 'test' || config.ADMINS?.includes(req.user_id),
 });
 
 // Rate limit modification requests to prevent abuse
@@ -123,7 +123,7 @@ const modificationLimiter = rateLimiter({
         debugLogger.info(`5 modifications/1m rate limit enforced on user ${req.user_id}`);
         res.status(429).send({ message: 'You have exceeded the allowed number of modifications per minute. Wait and try again.' });
     },
-    skip: req => req.method === 'OPTIONS' || req.method === 'GET' || process.env.NODE_ENV === 'test',
+    skip: req => req.method === 'OPTIONS' || req.method === 'GET' || process.env.NODE_ENV === 'test' || config.ADMINS?.includes(req.user_id),
 });
 
 // Super rate limit modification requests to stop bot abuse
@@ -136,7 +136,7 @@ const superModificationLimiter = rateLimiter({
         debugLogger.info(`100 modifications/1d rate limit enforced on user ${req.user_id}`);
         res.status(429).send({ message: 'You have exceeded the allowed number of modifications per day. Wait and try again.' });
     },
-    skip: req => req.method === 'OPTIONS' || req.method === 'GET' || process.env.NODE_ENV === 'test',
+    skip: req => req.method === 'OPTIONS' || req.method === 'GET' || process.env.NODE_ENV === 'test' || config.ADMINS?.includes(req.user_id),
 });
 
 // Parse body and cookies
