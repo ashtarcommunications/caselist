f7b4bd36113a2b68ebcc6d193c198428c4a22fa9
Aaron Hardy <ashtarcommunications@gmail.com>
2022-09-01 11:52:11 -0600

    Can't believe I made that mistake twice

--- server/v1/controllers/tabroom/postTabroomLink.js
+++ server/v1/controllers/tabroom/postTabroomLink.js
@@ -32,7 +32,7 @@ const postTabroomLink = {
         };
 
         try {
-            await fetch(url, { method: 'POST', headers: { 'Content-Type': 'application/json' }, body });
+            await fetch(url, { method: 'POST', headers: { 'Content-Type': 'application/json' }, body: JSON.stringify(body) });
         } catch (err) {
             debugLogger.error(`Failed to create Tabroom link to ${req.body.slug}: ${err}`);
             return res.status(500).json({ message: 'Failed to link to tabroom' });
