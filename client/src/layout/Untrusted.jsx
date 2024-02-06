25cbc820f04bf2842a56bebea9876f946b886634
Aaron Hardy <ashtarcommunications@gmail.com>
2024-02-05 21:36:31 -0700

    Wording for untrusted accounts

--- client/src/layout/Untrusted.jsx
+++ client/src/layout/Untrusted.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 
 const Untrusted = () => {
     let message = 'You must be a real student, judge, or coach on Tabroom to post on the caselist. ';
-    message += 'If you think this determination is in error, try waiting a few days and then log in again. ';
+    message += 'If you think this determination is in error, try logging out and back in. If your ';
+    message += 'account is brand new, try waiting a few days and then log in again. ';
     message += 'Until then, your account is in read-only mode, so you can browse, but not post.';
 
     return (
